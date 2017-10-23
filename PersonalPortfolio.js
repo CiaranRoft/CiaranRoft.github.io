f439c3bd96987d24b2ff6f573adb006743968360
CiaranRoft <ciaranroft@hotmail.com>
2017-10-23 17:48:43 +0100

    Adding profile picture, and nav bar styling

--- PersonalPortfolio.js
+++ PersonalPortfolio.js
@@ -1,17 +1,9 @@
-
 $(document).ready(function(){
-	// First Check (Handles color of the nav-bar if the page is reloaded half way down the page)
- 	if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-      	$(".navbar").css("background-color", "#D4CAD5"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
- 	} else {
-      	$(".navbar").css("background-color", "transparent"); // if not, change it back to transparent
-    }
-
-  	$(window).scroll(function() { // check if scroll event happened
-    	if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-      		$(".navbar").css("background-color", "#D4CAD5"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-    	} else {
-      	$(".navbar").css("background-color", "transparent"); // if not, change it back to transparent
-    	}
-  	});
+    // Code to make the hamburger retract after option is selected
+    $('.nav a').on('click', function(){ 
+      console.log("Link clciked!!");
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
 });
